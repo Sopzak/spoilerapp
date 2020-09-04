1752f9eb5f583a268f9f6e2b644b9dbcc063cbe7
Jesiel <je.sopzak@gmail.com>
2020-09-04 06:16:30 -0300

    Hello word

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Word.<br/>Hello everone!
+          Hello Word.
         </p>
       </header>
     </div>
